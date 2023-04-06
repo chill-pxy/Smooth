4f20fc0a57a209a1190b31baf9da59a05878a62c
pxy <1321337557@qq.com>
2023-04-06 15:57:20 +0800

    config_manager update

---
+++ engine/source/runtime/resource/config_manager/config_manager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <filesystem>
+
+namespace Smooth
+{
+    class ConfigManager
+    {
+    public:
+        void initialize(const std::filesystem::path& config_file_path);
+
+        const std::filesystem::path& getRootFolder() const;
+        const std::filesystem::path& getAssetFolder() const;
+        const std::filesystem::path& getEditorBigIconPath() const;
+        const std::filesystem::path& getEditorSmallIconPath() const;
+        const std::filesystem::path& getEditorFontPath() const;
+    
+    private:
+        std::filesystem::path m_root_folder;
+        std::filesystem::path m_asset_folder;
+        std::filesystem::path m_editor_big_icon_path;
+        std::filesystem::path m_editor_small_icon_path;
+        std::filesystem::path m_editor_font_path;
+    }
+}
